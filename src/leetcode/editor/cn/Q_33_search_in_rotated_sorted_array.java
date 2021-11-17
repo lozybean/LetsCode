69bcfa2e5b40be75e59463ccde296993e5ddd46f
liangzebin <liangzebin5566@gmail.com>
2021-11-17 19:36:40 +0800

    Q33 Q200

---
+++ src/leetcode/editor/cn/Q_33_search_in_rotated_sorted_array.java
@@ -0,0 +1,90 @@
+package leetcode.editor.cn.q_33;
+
+import leetcode.editor.cn.def.*;
+
+import java.util.*;
+
+//整数数组 nums 按升序排列，数组中的值 互不相同 。 
+//
+// 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[
+//k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2
+//,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。 
+//
+// 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [4,5,6,7,0,1,2], target = 0
+//输出：4
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [4,5,6,7,0,1,2], target = 3
+//输出：-1 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1], target = 0
+//输出：-1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 5000 
+// -10^4 <= nums[i] <= 10^4 
+// nums 中的每个值都 独一无二 
+// 题目数据保证 nums 在预先未知的某个下标上进行了旋转 
+// -10^4 <= target <= 10^4 
+// 
+//
+// 
+//
+// 进阶：你可以设计一个时间复杂度为 O(log n) 的解决方案吗？ 
+// Related Topics 数组 二分查找 👍 1671 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        if (nums.length == 1) return nums[0] == target ? 0 : -1;
+        if (nums[0] == target) return 0;
+        if (nums[nums.length - 1] == target) return nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[0] <= nums[mid]) {
+                // 旋转位置在mid右侧
+                if (nums[0] < target && target < nums[mid]) {
+                    // 在左侧
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // 旋转位置在mid左侧
+                if (nums[mid] < target && target < nums[0]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
